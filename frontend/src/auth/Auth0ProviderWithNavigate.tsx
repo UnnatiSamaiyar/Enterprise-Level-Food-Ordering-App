5fa7c26319f302057b1c1386f11645498ec87ec5
UnnatiSamaiyar <unnatis2910btechcse2025@kccitm.edu.in>
2024-06-07 02:17:14 +0530

    token auth

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,13 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
 }
 
 const Auth0ProviderWithNavigate = ({children}: Props) => {
+    const navigate = useNavigate();
+    const {getAccessTokenSilently} = useAuth0();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -12,8 +15,9 @@ const Auth0ProviderWithNavigate = ({children}: Props) => {
     if (!domain || !clientId || !redirectUri) {
         throw new Error("unable to initialise auth");
     }
-    const onRedirectCallback = (appState?: AppState, user?: User) =>{
-        console.log("USER", user);
+    const onRedirectCallback = () =>{
+        
+        navigate("/auth-callback");
     }
     return (
         <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{ redirect_uri: redirectUri }} onRedirectCallback={onRedirectCallback}>{children}</Auth0Provider>
